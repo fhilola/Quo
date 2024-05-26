831de82cb74fc6fd93b048fc6b7f680985cce8fd
fhilola <hilolafayoziddinova14@gmail.com>
2024-05-26 16:44:08 +0500

    routes added

--- quo/src/App.jsx
+++ quo/src/App.jsx
@@ -2,13 +2,14 @@
 import './App.css'
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
      <Navbar/>
      <Hero/>
-    </>
+    </BrowserRouter>
   )
 }
 
---
+++ quo/src/routes/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RouteController = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<App />}/>
+    </Routes>
+  )
+}
+
+export default RouteController
